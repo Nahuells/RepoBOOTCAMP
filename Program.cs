2eec378ce986c8fc9e5daad22a6541d42c4cc762
Nahuells <N.l1234567>
2022-06-28 18:16:28 -0300

    Ejer1y2

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/*Ejercicio 1
+Escribe un programa que reciba tu nombre y lo escriba por consola.
+
+Ejercicio 2
+Escribe un programa que tome la hora y la escriba por pantalla.*/
+
+Console.WriteLine("Introduse tu nombre:");
+
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Hola," + nombre);
+
+Console.WriteLine("introduse la hora:");
+
+string hora = Console.ReadLine();
+
+Console.WriteLine("La hora introducida fue," + hora);
